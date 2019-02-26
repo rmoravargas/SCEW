1fe523dac141bdb446620813e1a4bf4f6142bf19
Ruben Mora Vargas <rmoravargas@hotmail.com>
2019-02-25 22:14:42 -0600

    Se completa la sección llamada search

--- sce-web/src/app/components/search/search.component.ts
+++ sce-web/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
-
+  list: any[];
+  showedList: any[]=[];
+  constructor(private dataService: DataService) {
+    this.list = dataService.getCoursesList();
+    this.showedList = this.list;
+  }
   ngOnInit() {
+  }
+  searchCourses = (term: string) => {
+    if (!term) {
+      this.showedList = [];
+    } else { 
+      term=term.toLowerCase();
+      this.showedList = [];
+      this.list.forEach(  (item) => { 
+        if (item.nombre.toLowerCase().includes(term)) {
+          this.showedList.push(item);
+        }
+      });
+    }
+
   }
 
 }
--- sce-web/src/app/services/data/data.service.ts
+++ sce-web/src/app/services/data/data.service.ts
@@ -2,81 +2,132 @@ import { Injectable } from '@angular/core';@Injectable({
   providedIn: 'root'
 })
 export class DataService {  
+
   private  estudentsList = [{
     "nombre": "Carlos Mora",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "Angela García",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "Cristhian Brenes",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "8"
 },
 {
     "nombre": "Luis Rodriguez",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "Alejandra Godinez",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "10"
 },
 {
     "nombre": "Esteban Saenz",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "Maria Cordero",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "10"
 },
 {
     "nombre": "Angel Vargas",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "Pedro Escobar",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "8"
 },
 {
     "nombre": "Jaime Celeita",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9.5"
 },
 {
     "nombre": "Ileana Salas",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "9"
 },
 {
     "nombre": "",
     "curso": "Programación Web",
-    "año": 2019,
+    "anyo": 2019,
     "nota": "10"
 }
 ];
 
+ 
+  private  coursesList = [
+    {
+    "nombre": "Programación Web",
+    "profesor": "Randall Corrales",
+    "anyo": 2019,
+    "semestre": "I"
+},
+{
+  "nombre": "Análisis en cloud Computing",
+  "profesor": "Jorge Chinchilla",
+  "anyo": 2019,
+  "semestre": "II"
+},
+{
+  "nombre": "Investigación de proyectos",
+  "profesor": "Carlos Benavides",
+  "anyo": 2019,
+  "semestre": "II"
+},
+{
+  "nombre": "Sistemas de información distribuidos",
+  "profesor": "Ana Fuentes",
+  "anyo": 2019,
+  "semestre": "I"
+},
+{
+  "nombre": "Bases de datos Web",
+  "profesor": "Krissia Navarro",
+  "anyo": 2019,
+  "semestre": "I"
+},
+{
+  "nombre": "Diseño Web",
+  "profesor": "Kembly Calderon",
+  "anyo": 2019,
+  "semestre": "II"
+},
+{
+  "nombre": "Analisis de Bases de datos",
+  "profesor": "Jorge Mora",
+  "anyo": 2019,
+  "semestre": "II"
+}
+];
+
+
+
 constructor() { }
 
 getStudentList =() => this.estudentsList;
+
+getCoursesList =() => this.coursesList;
 }
